860d5e0748502cf1bfde5188ac50eceb0f72fe51
Emmanuel <emmatorres211993@gmail.com>
2024-02-03 22:16:31 -0300

    ultimos cambios

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+const socket = io();
+
+socket.emit("message", "¡Hola, me estoy comunicando desde un websocket!")
+
+
+socket.on("evento_para_socket_individual", (data) => console.log(data));
+
+socket.on("evento_para_todos_menos_el_socket_actual", (data) => console.log(data));
+
+socket.on("evento_para_todos", (data) => console.log(data))
